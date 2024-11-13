fa8dddfeaaaf48ff32e4d8e807c938462b6a02c6
dog-in-space <github.ferilli@simplelogin.com>
2024-11-13 05:08:33 -0500

    Fixed hostname values

--- dot_config/private_fish/conf.d/hostname-color.fish
+++ dot_config/private_fish/conf.d/hostname-color.fish
@@ -1,11 +1,16 @@
+
+if set -q hostname
+	set -x HOSTNAME "$hostname"
+end
+
 if status is-interactive
 
-if test "$hostname" = "M1-macOS"
+if test "$HOSTNAME" = "M1-macOS"
 	set fish_color_host FA9D0E
 	set fish_color_user white
 else if test "$HOSTNAME" = "pink-alma"
 	set fish_color_host FA7DF0
-else if test "$HOSTNAME" = "q58-gpc"
+else if test "$HOSTNAME" = "Q58-GPC"
 	set fish_color_hosts 2E0C5E
 else 
 
