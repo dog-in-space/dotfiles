4dacebaeada83e194acf1388ff7ea0de76f09af1
dog-in-space <github.ferilli@simplelogin.com>
2024-11-22 20:48:51 -0500

    Updates

--- dot_config/private_fish/conf.d/hostname-color.fish
+++ dot_config/private_fish/conf.d/hostname-color.fish
@@ -5,22 +5,28 @@ end
 
 if status is-interactive
 
-if test "$HOSTNAME" = "M1-macOS"
+if test "$HOSTNAME" = "orange"
 	set fish_color_host FA9D0E
 	set fish_color_host_remote FA9D0E
 	set fish_color_user white
-else if test "$HOSTNAME" = "pink-alma"
+else if test "$HOSTNAME" = "pink"
 	set fish_color_host FA7DF0
 	set fish_color_host_remote FA7DF0
 	set fish_color_user white	
-else if test "$HOSTNAME" = "Q58-GPC"
-	set fish_color_hosts 2E0C5E
+else if test "$HOSTNAME" = "purple"
+	set fish_color_host 2E0C5E
 	set fish_color_host_remote 2E0C5E
+	set fish_color_user white
+else if test "$HOSTNAME" = "blue"
+	set fish_color_hosts blue
+	set fish_color_host_remote blue
 	set fish_color_user white
 else 
 	set fish_color_host green
 	set fish_color_host_remote green
 	set fish_color_user white
 end
+
+set fish_color_autosuggestion white
 
 end
