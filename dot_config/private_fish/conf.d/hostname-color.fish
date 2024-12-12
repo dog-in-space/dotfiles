8e478ff9222b21cb6a831ae84e0d9c06ff47781b
dog-in-space <github.ferilli@simplelogin.com>
2024-12-12 12:41:11 -0500

    fix 🌈: hostname colors

--- dot_config/private_fish/conf.d/hostname-color.fish
+++ dot_config/private_fish/conf.d/hostname-color.fish
@@ -5,33 +5,23 @@ end
 
 if status is-interactive
 
-if test "$HOSTNAME" = "orange"
-	set fish_color_host FA9D0E
-	set fish_color_host_remote FA9D0E
-if test "$HOSTNAME" = "M1-macOS"
-	set fish_color_host yellow
-	set fish_color_user white
-else if test "$HOSTNAME" = "pink"
-	set fish_color_host FA7DF0
-	set fish_color_host_remote FA7DF0
-	set fish_color_user white	
-else if test "$HOSTNAME" = "purple"
-	set fish_color_host 2E0C5E
-	set fish_color_host_remote 2E0C5E
-	set fish_color_user white
-else if test "$HOSTNAME" = "blue"
-	set fish_color_hosts blue
-	set fish_color_host_remote blue
-	set fish_color_user white
-else if test "$HOSTNAME" = "Q58-GPC"
-	set fish_color_host magenta
-	set fish_color_user white
-else 
-	set fish_color_host green
-	set fish_color_host_remote green
-	set fish_color_user white
+	if test "$HOSTNAME" = "orange"
+		set fish_color_host FA9D0E
+		set fish_color_host_remote FA9D0E
+	else if test "$HOSTNAME" = "pink"
+		set fish_color_host FA7DF0
+		set fish_color_host_remote FA7DF0
+	else if test "$HOSTNAME" = "purple"
+		set fish_color_host EA00DE 
+		set fish_color_host_remote EA00DE
+	else if test "$HOSTNAME" = "blue"
+		set fish_color_hosts blue
+		set fish_color_host_remote blue
+	else 
+		set fish_color_host green
+		set fish_color_host_remote green
+	end
 end
 
-set fish_color_autosuggestion white
+	set fish_color_autosuggestion white
 
-end
