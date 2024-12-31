05404a99ef4276b06983274a2e40d06cc1795adf
dog-in-space <github.ferilli@simplelogin.com>
2024-12-31 03:03:12 -0500

    change: renamed orange to silver

--- dot_config/private_fish/conf.d/hostname-color.fish
+++ dot_config/private_fish/conf.d/hostname-color.fish
@@ -6,9 +6,9 @@ end
 
 if status is-interactive
 
-if test "$HOSTNAME" = "orange"
-	set fish_color_host FA9D0E
-	set fish_color_host_remote FA9D0E
+if test "$HOSTNAME" = "silver"
+	set fish_color_host 8A8D8F
+	set fish_color_host_remote 8A8D8F
 else if test "$HOSTNAME" = "pink"
 	set fish_color_host FA7DF0
 	set fish_color_host_remote FA7DF0
