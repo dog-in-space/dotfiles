00ee289f1c703fa70e831a44b5646ed76ef647d9
dog-in-space <github.ferilli@simplelogin.com>
2024-12-31 03:51:44 -0500

    feat: track cli packages in a variable

---
+++ dot_config/private_fish/conf.d/packages.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/env fish
+
+set -x BREW_PACKAGES_CLI aichat bat chezmoi dockutil duf dust eza fastfetch fd fish fisher fzf gh git just mas most neovim podman-tui progress syncthing thefuck tlrc trash-cli zoxide zsh
+
+if test (uname) = "Darwin"
+    set -x -a BREW_PACKAGES_CLI dockutil mas syncthing
+end
+
+alias brew-up="brew install $BREW_PACKAGES_CLI"
